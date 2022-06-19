195e1768a191b7f89d5e62ccf818d27ad47de51f
Rafael Munareto <47766522+RafaelMunareto@users.noreply.github.com>
2022-06-19 17:36:40 -0300

    settings e grupo controller

---
+++ src/modules/settings/settings.controller.ts
@@ -0,0 +1,58 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
+import { Settings } from 'http2';
+import { SettingsDto } from './dto/settings.dto';
+import { SettingsService } from './settings.service';
+
+@ApiTags('Settings')
+@UseGuards(AuthGuard('jwt'))
+@ApiBearerAuth()
+@Controller('settings')
+export class SettingsController {
+  constructor(private readonly settingsService: SettingsService) {}
+
+  @Get()
+  @ApiForbiddenResponse({ description: 'Acesso negado' })
+  @HttpCode(HttpStatus.OK)
+  public async index(): Promise<Settings[]> {
+    return this.settingsService.index();
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  public async show(@Param('id') id: string) {
+    return this.settingsService.show(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  public async store(@Body() settingsDto: SettingsDto): Promise<Settings> {
+    return this.settingsService.store(settingsDto);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  public async update(
+    @Param('id') id: String,
+    @Body() settingsDto: SettingsDto,
+  ): Promise<Settings> {
+    return this.settingsService.update(settingsDto, id);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  public async delete(@Param('id') id: string) {
+    return this.settingsService.delete(id);
+  }
+}
